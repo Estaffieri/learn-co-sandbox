14f1c3ab6bb9e2eff3a62e1bc056115d8153d8bb
Estelle S <estellelareinne@gmail.com>
2019-11-17 17:08:54 +0000

    Automatically backed up by Learn

---
+++ greeting.rb
@@ -0,0 +1,14 @@
+def greeting_programmer(name, language = "computer")
+  puts "Hello, #{name}. We heard you are a great #{language} programmer."
+end
+ 
+greeting_programmer("Steven", "Ruby")
+
+greeting_programmer ("Maria")
+
+def greeting (name)
+  puts "Hello, #{name}."
+end  
+
+greeting("Estelle")
+
---
+++ learning-comments.rb
@@ -0,0 +1,10 @@
+chance_of_rain = -23
+puts "let's go outside!"
+if chance_of_rain > 0.5
+  puts "Pack an Umbrella!"
+else
+  puts "Enjoy the fine day!"
+end
+puts "Oh, and always wear sunscreen!"
+
+Time.now.year
